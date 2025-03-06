e93cc39ccd26b46494b0c386f8f64d701857c011
LindaTancini <lindatancini95@gmail.com>
2025-03-06 14:43:00 +0100

    feat: added "document.getElementById"

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// PRENDO GLI ELEMENTI IN PAGINA
+// ID COUNTDOWN
+const countdownElement = document.getElementById("countdown");
+console.log(countdownElement);
+// ID NUMBER-LIST
+const numberElement = document.getElementById("numbers-list");
+console.log(numberElement);
+// ID ANSWERS-FORM
+const answersElement = document.getElementById("answers-form");
+console.log(answersElement);
